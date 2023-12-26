340cf271488d4804dc53b794848f4e025392b56d
orutanakaito <voltgt777@gmail.com>
2023-12-26 19:52:10 +0900

    再度コミットやり直しました。

--- src/main/java/dungeon_rpg/GameStart.java
+++ src/main/java/dungeon_rpg/GameStart.java
@@ -4,7 +4,7 @@ import dungeon_rpg.battle.Battle;
 import dungeon_rpg.magic.*;
 import dungeon_rpg.map.MapManager;
 import dungeon_rpg.monster.Boss;
-import dungeon_rpg.monster.Slime;
+import dungeon_rpg.monster.Monster;
 import dungeon_rpg.player.Player;
 
 import java.util.Random;
@@ -13,7 +13,7 @@ import java.util.Random;
 public class GameStart {
 
     Player player = new Player("勇者", 100, 1, 50, 0, 0, 0, 0);
-    Slime slime = new Slime("スライム", 200, 1, 0, 0);
+    Monster monster = new Monster("スライム", 200, 1, 0, 0);
     Boss boss = new Boss("魔王", 300, 0, 20, 0);
     Battle battle = new Battle();
     Position current = new Position(1, 1);
@@ -28,27 +28,27 @@ public class GameStart {
     }
 
     public void battleLog() {
-        for (int i = 0; slime.getHp() > 0; i++) {
-            if (slime.getHp() != 0) {
-                battle.playerAttackDamage(player, slime);
-                battle.magicAttack(MagicType.FIRE, player, slime);
-                battle.magicAttack(MagicType.THUNDER, player, slime);
-                battle.magicAttack(MagicType.BLIZZARD, player, slime);
-                if (slime.getHp() <= 0) {
-                    slime.Hp0();
+        for (int i = 0; monster.getHp() > 0; i++) {
+            if (monster.getHp() != 0) {
+                battle.playerAttackDamage(player, monster);
+                battle.magicAttack(MagicType.FIRE, player, monster);
+                battle.magicAttack(MagicType.THUNDER, player, monster);
+                battle.magicAttack(MagicType.BLIZZARD, player, monster);
+                if (monster.getHp() <= 0) {
+                    monster.Hp0();
                     break;
                 }
-                battle.playerCriticalHit(player, slime);
-                if (slime.getHp() <= 0) {
-                    slime.Hp0();
+                battle.playerCriticalHit(player, monster);
+                if (monster.getHp() <= 0) {
+                    monster.Hp0();
                     break;
                 }
-                battle.monsterAttackDamage(player, slime);
+                battle.monsterAttackDamage(player, monster);
                 if (player.getHp() <= 0) {
                     player.Hp0();
                     break;
                 }
-                battle.monsterLargeDamage(player, slime);
+                battle.monsterLargeDamage(player, monster);
                 if (player.getHp() <= 0) {
                     player.Hp0();
                     break;
@@ -59,12 +59,12 @@ public class GameStart {
             System.out.println("勇者は勝利した！");
             battle.training(player);
             player = new Player("勇者", player.getHp(), player.getLv(), player.getMp(), 0, 0, 0, player.getGold());
-            slime = new Slime("スライム", 200, 1, 0, 0);
+            monster = new Monster("スライム", 200, 1, 0, 0);
         } else {
             System.out.println("勇者は負けた！");
             mapM.current = new Position(1, 1);
             player = new Player("勇者", 100, 1, 50, 0, 0, 0, 0);
-            slime = new Slime("スライム", 200, 1, 0, 0);
+            monster = new Monster("スライム", 200, 1, 0, 0);
         }
     }
 
@@ -91,7 +91,7 @@ public class GameStart {
             System.out.println("勇者は魔王に負けました！");
             mapM.current = new Position(1, 1);
             player = new Player("勇者", 100, 1, 50, 0, 0, 0, 0);
-            slime = new Slime("スライム", 200, 1, 0, 0);
+            monster = new Monster("スライム", 200, 1, 0, 0);
         }
     }
 
---
+++ src/main/java/dungeon_rpg/map/MapManager.java
@@ -0,0 +1,160 @@
+package dungeon_rpg.map;
+
+import dungeon_rpg.GameStart;
+import dungeon_rpg.Position;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MapManager {
+    GameStart gs;
+    public Map mapTile = new Map();
+    public Position current = new Position(1, 1);
+    public Position current2 = new Position(6, 1);
+    public Position goal = new Position(6, 1);
+    public Position goal2 = new Position(1, 0);
+
+    public MapManager(GameStart gs) {
+        this.gs = gs;
+    }
+
+    public void maze() {
+
+        for (; ; ) {
+
+            // 迷路の表示
+            for (int col = 0; col < mapTile.map.length; col++) {
+
+                for (int row = 0; row < mapTile.map[col].length; row++) {
+
+                    if (row == current.getRow() && col == current.getCol()) {
+                        System.out.print("P");
+
+                    } else if (mapTile.map[col][row] == 1) {
+                        System.out.print("■");
+
+                    } else if (mapTile.map[col][row] == 4) {
+                        System.out.print("B");
+
+                    } else if (mapTile.map[col][row] == 5) {
+                        System.out.print("H");
+
+                    } else {
+                        System.out.print(".");
+                    }
+                }
+                System.out.println();
+            }
+
+            // ゴール判定
+            if (current.equals(goal)) {
+                System.out.println("ステージ2!");
+                break;
+            }
+            update();
+        }
+    }
+
+    public void maze2() {
+
+        for (; ; ) {
+
+            // 迷路の表示
+            for (int col = 0; col < mapTile.map2.length; col++) {
+
+                for (int row = 0; row < mapTile.map2[col].length; row++) {
+
+                    if (row == current2.getRow() && col == current2.getCol()) {
+                        System.out.print("P");
+
+                    } else if (mapTile.map2[col][row] == 3) {
+                        System.out.print("▲");
+
+                    } else if (mapTile.map2[col][row] == 2) {
+                        System.out.print(".");
+                    }
+                }
+                System.out.println();
+            }
+
+            // ゴール判定
+            if (current2.equals(goal2)) {
+                System.out.println("GOAL!!");
+                break;
+            }
+            update2();
+        }
+    }
+
+    public void update() {
+
+        // キー入力処理
+
+        Scanner ch = new Scanner(System.in);
+        String str = ch.nextLine();
+
+        // 押された方向の座標を得る
+        Position next = switch (str) {
+            case "a" -> new Position(current.getRow() - 1, current.getCol());
+            case "w" -> new Position(current.getRow(), current.getCol() - 1);
+            case "s" -> new Position(current.getRow() + 1, current.getCol());
+            case "z" -> new Position(current.getRow(), current.getCol() + 1);
+            default -> current;
+        };
+
+        // 押された方向が通路なら進む
+        if (mapTile.map[next.getCol()][next.getRow()] == 0) {
+            current = next;
+        }
+        int randomProbability = 5;
+        Random random = new Random();
+        if ((random.nextInt(randomProbability) + 1) == 1) {
+            if (mapTile.map[next.getCol()][next.getRow()] == 0) {
+                current = next;
+                System.out.println("勇者はモンスターに遭遇した！");
+                gs.battleLog();
+            }
+        }
+        if (mapTile.map[next.getCol()][next.getRow()] == 4) {
+            current = next;
+            System.out.println("勇者は魔王と決戦！");
+            gs.bossBattlelog();
+        }
+        if (mapTile.map[next.getCol()][next.getRow()] == 5) {
+            current = next;
+            System.out.println("回復ポイント！");
+            gs.recovery();
+        }
+    }
+
+    public void update2() {
+
+        Scanner ch = new Scanner(System.in);
+        String str2 = ch.nextLine();
+
+        Position next2 = switch (str2) {
+            case "a" -> new Position(current2.getRow() - 1, current2.getCol());
+            case "w" -> new Position(current2.getRow(), current2.getCol() - 1);
+            case "s" -> new Position(current2.getRow() + 1, current2.getCol());
+            case "z" -> new Position(current2.getRow(), current2.getCol() + 1);
+            default -> current2;
+        };
+
+        if (mapTile.map2[next2.getCol()][next2.getRow()] == 2) {
+            current2 = next2;
+        }
+        int randomProbability = 5;
+        Random random = new Random();
+        if ((random.nextInt(randomProbability) + 1) == 1) {
+            if (mapTile.map2[next2.getCol()][next2.getRow()] == 2) {
+                current2 = next2;
+                System.out.println("勇者はモンスターに遭遇した！");
+                gs.battleLog();
+            }
+        }
+    }
+}
+
+
+
+
